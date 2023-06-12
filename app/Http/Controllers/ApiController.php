7b41fe11cc2d90f44cc0acb51a23b7d38d621ae4
StitaMaharani21 <stitamaharanigst@gmail.com>
2023-06-12 11:51:41 +0700

    Api update

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Portofolio;
+use App\Http\Requests\AddPortofolioRequest;
+use App\Http\Requests\UpdatePortofolioRequest;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $portofolio = Portofolio::all();
+        return response($portofolio);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(AddPortofolioRequest $request)
+    {
+        if ($request->hasFile('image_url')) {
+            $imagePath = $request->file('image_url')->store('uploads', 'public');
+        } else {
+            $imagePath = '';
+        }
+        $portofolio = new Portofolio;
+        $portofolio->category_id = $request->category;
+        $portofolio->title = $request->title;
+        $portofolio->description = $request->description;
+        $portofolio->image_url = '/storage/' . $imagePath;
+        $portofolio->save();
+
+        return response('add portfolio success');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePortofolioRequest $request, string $id)
+    {
+        if ($request->hasFile('image_url')) {
+            $imagePath = $request->file('image_url')->store('uploads', 'public');
+        } else {
+            $imagePath = '';
+        }
+
+        $portofolio = Portofolio::findOrFail($id);
+        Storage::delete('uploads/' . $portofolio->image_url);
+        $portofolio->category_id = $request->category;
+        $portofolio->title = $request->title;
+        $portofolio->description = $request->description;
+        $portofolio->image_url = '/storage/' . $imagePath;
+        $portofolio->save();
+
+        return response('update portfolio success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $portofolio = Portofolio::findOrFail($id);
+        $portofolio->delete();
+
+        return response('delete portfolio success');
+    }
+
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('portfolios', [ApiController::class, 'index']);
+Route::post('portfolios', [ApiController::class, 'store']);
+Route::put('portfolios/{id}', [ApiController::class, 'update']);
+Route::delete('portfolios/{id}', [ApiController::class, 'destroy']);
